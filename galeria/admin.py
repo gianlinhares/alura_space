dcb29ae69b506ac9be12be08b19d033deef53d43
Gianluca Linhares <gian.linhares@hotmail.com>
2025-10-03 13:27:42 -0300

    Melhorias no projeto

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from galeria.models import Fotografia
+    
+class ListandoFotografia(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "publicada")
+    list_display_links = ("id", "nome")
+    list_filter = ("id", "nome")
+    search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_editable = ("publicada",)
+    list_per_page = 10
 
-# Register your models here.
+admin.site.register(Fotografia, ListandoFotografia) 
